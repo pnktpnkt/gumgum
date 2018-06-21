8aa6e9edea64f9f7a909d9768c0393b43f52d541
pnktpnkt <noglasses1215@gmail.com>
2018-06-21 19:49:04 +0900

    modify README.md

--- Assets/InvisibleAvatarScript.cs
+++ Assets/InvisibleAvatarScript.cs
@@ -9,19 +9,28 @@ public class InvisibleAvatarScript : MonoBehaviour
 	private Transform rightHandTarget;
 	private Vector3 handAccel;
 	private float headToHandDist;
+	public bool leftController = false;
 
 	// Use this for initialization
 	void Start () {
 		headTarget = vrik.solver.spine.headTarget.transform;
 		rightHandTarget = vrik.solver.rightArm.target.transform;
 		headToHandDist = (rightHandTarget.position - headTarget.position).sqrMagnitude;
-		handAccel = OVRInput.GetLocalControllerAcceleration (OVRInput.Controller.RTouch);
+		if (!leftController) {
+			handAccel = OVRInput.GetLocalControllerAcceleration (OVRInput.Controller.RTouch);
+		} else {
+			handAccel = OVRInput.GetLocalControllerAcceleration (OVRInput.Controller.LTouch);
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
 		headToHandDist = (rightHandTarget.position - headTarget.position).sqrMagnitude;
-		handAccel = OVRInput.GetLocalControllerAcceleration (OVRInput.Controller.RTouch);
+		if (!leftController) {
+			handAccel = OVRInput.GetLocalControllerAcceleration (OVRInput.Controller.RTouch);
+		} else {
+			handAccel = OVRInput.GetLocalControllerAcceleration (OVRInput.Controller.LTouch);
+		}
 	}
 
 	public Vector3 getHandAccel(){
